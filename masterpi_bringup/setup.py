350256ffde1ec11847fc3158fee893dfd147be2b
Alfr3do10 <a01737250@tec.mx>
2026-05-27 18:14:59 -0600

    Cambio en Camara_Node para usar Camera.SDK y usar la calibración

--- masterpi_bringup/setup.py
+++ masterpi_bringup/setup.py
@@ -7,7 +7,11 @@ package_name = 'masterpi_bringup'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    # CAMBIO AQUÍ: Añadimos la subcarpeta del SDK explícitamente usando puntos
+    packages=[
+        package_name,
+        f'{package_name}.hiwonder_sdk'
+    ],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
